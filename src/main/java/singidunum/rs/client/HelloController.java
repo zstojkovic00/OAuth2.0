e3f05c0cd877241e353712c41d892e5fed760064
zstojkovic00 <00zeljkostojkovic@gmail.com>
2024-03-03 19:03:36 +0100

    Implemented communication with Okta authorization server to retrieve the authorization code

--- src/main/java/singidunum/rs/client/HelloController.java
+++ src/main/java/singidunum/rs/client/HelloController.java
@@ -2,25 +2,41 @@ package singidunum.rs.client;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
+import java.util.UUID;
 
 
 @Controller
 public class HelloController {
+    @Value("${client.id}")
+    private String clientId;
+    @Value("${client.secret.id}")
+    private String clientSecret;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model){
+    public String index(HttpServletRequest request, Model model) {
         var session = request.getSession();
-        if(session != null && session.getAttribute("username") != null){
+        if (session != null && session.getAttribute("username") != null) {
             model.addAttribute("username", session.getAttribute("username"));
             model.addAttribute("attributes", session.getAttribute("attributes"));
             return "authenticated";
         } else {
+            var loginUri = UriComponentsBuilder.fromHttpUrl("https://dev-80556277.okta.com/oauth2/default/v1/authorize")
+                    .queryParam("redirect_uri", "http://localhost:8080/oauth2/callback")
+                    .queryParam("response_type", "code")
+                    .queryParam("state", UUID.randomUUID())
+                    .queryParam("scope", "openid email profile")
+                    .queryParam("client_id", clientId)
+                    .build();
+            model.addAttribute("loginUri", loginUri);
+
             return "anonymous";
         }
     }
